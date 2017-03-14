aaadbab545a64a86f45b416c673db27e7387ccba
Jesus Domenech <jesusjda@gmail.com>
2017-03-14 13:34:23 +0100

    demo FASE17 ready

--- bin/demo/mhp1.sh
+++ bin/demo/mhp1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$EI_TMP=$1/_ei_tmp
-MHP_HOME=/home/friker/Systems/costa/costabs/src/interfaces/mhp/shell
+EI_TMP=$1/_ei_tmp
+. envisage/envisage_settings.sh
+. misc/parse_params.sh
 
+PROGRAM=/home/friker/Systems/costa/costabs/src/interfaces/mhp/shell/mhp_shell
 
-
-$MHP_HOME/mhp_shell main -i '../examples/PeerToPeer.abs' -v 2  -l -dot $EI_TMP/graph.dot -ei_version 2
-$MHP_HOME/mhp_shell main -i '../examples/PeerToPeer.abs' -v 1  -l -dot $EI_TMP/graph.dot
-
+echo ""
+${PROGRAM} 'main' $@ 
 
---
+++ bin/demo/mhp2.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+### A wrapper script for SACO's MHP analysis
+###
+###
+
+EI_TMP=$1/_ei_tmp
+# execute envisage_settings.sh to set some environment variables needed by
+# costabs, etc.
+#
+. envisage/envisage_settings.sh
+. misc/parse_params.sh
+
+PROGRAM=/home/friker/Systems/costa/costabs/src/interfaces/mhp/shell/mhp_shell
+
+# Decide the entry. If the user provide a single entry then use it,
+# otherwise use 'main'
+#
+entries=$(getparam "entries")
+numofentries=($entries)
+
+if [ ${#numofentries[@]} == 1 ]; then
+    entry="$entries"
+else
+    entry="main"
+fi
+
+# remove -entries and its arguments, we assume it is the first in the
+# command-line parameters
+#
+shift                      
+shift ${#numofentries[@]} 
+shift
+
+# Execute mh_shell, we add '-ei_version 2' to the parameters since now
+DEMO=2
+${PROGRAM} $entry $@ -ei_version 2 -dot $EI_TMP/graph.dot -highlight '/dev/null' &> /tmp/mhp.stderr
+
+
+
+# If costabs exit with exit-code 0 we just print the output to the
+# stdout, otherwise we print an error message to the stdout as well.
+#
+if [ $? == 0 ]; then
+    echo "<eiout>"
+    echo "<eicommands>"
+    if [ -f $EI_TMP/graph.dot ]; then
+	sed -i -e "s/border=0,//g" $EI_TMP/graph.dot 
+	dot -Tsvg < $EI_TMP/graph.dot > $EI_TMP/graph.svg
+	echo "<printonconsole consoleid='Graph'>"
+	echo "<content format='svg'>"
+	echo "<svg width='1000pt' height='500pt' "
+	tail -n +8 $EI_TMP/graph.svg
+	echo "</content>"
+	echo "</printonconsole>"
+	echo ""
+    fi
+    tail -n +4 /tmp/costabs/output.xml
+
+
+else
+    echo "<eiout>"
+    echo "<eicommands>"
+    echo "<printonconsole consoleid='Error'>"
+    echo "<content format='text'>"
+    cat /tmp/mhp.stderr
+    echo "</content>"
+    echo "</printonconsole>"
+    echo "<dialogbox boxtitle='Execution Error' boxwidth='400'>"
+    echo "<content format='html'>"
+    echo "<span style='color:red;' >Error occurred while executing May-Happen-in-Parallel analysis:</span>"
+    echo "</content>"
+    echo "</dialogbox>"
+    echo "</eicommands>"
+    echo "</eiout>"
+fi
+
+\rm -f /tmp/mhp.stderr
+#rm -f /tmp/costabs/output.xml
+
