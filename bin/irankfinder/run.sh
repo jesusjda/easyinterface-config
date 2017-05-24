10ffdd8fdf1889882d647efdfd4d198e639c7b80
Jesus Domenech <jesusjda@gmail.com>
2017-05-24 11:17:00 +0200

    minor

--- bin/irankfinder/run.sh
+++ bin/irankfinder/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-rm -rf /home/friker/Systems/irankfinder/trunk/build/*
-find /home/friker/Systems/irankfinder/trunk -iname *.java > files-java.txt
+TMPDIR=$1/_ei_tmp
+# rm -rf /home/friker/Systems/irankfinder/build/*
+find /home/friker/Systems/irankfinder -iname *.java > $TMPDIR/files-java.txt
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/:/usr/local/lib/ppl/
 export LD_LIBRARY_PATH
-javac -cp /usr/local/lib/ppl/ppl_java.jar:/home/friker/Systems/irankfinder/trunk/lib/commons-cli-1.3.1.jar -d /home/friker/Systems/irankfinder/trunk/build @files-java.txt
-java  -cp /home/friker/Systems/irankfinder/trunk/lib/commons-cli-1.3.1.jar:/home/friker/Systems/irankfinder/trunk/build:/usr/local/lib/ppl/ppl_java.jar loopkiller.irankfinder.Main $@
+mkdir $TMPDIR/build
+javac -cp /usr/local/lib/ppl/ppl_java.jar:/home/friker/Systems/irankfinder/lib/commons-cli-1.3.1.jar -d $TMPDIR/build @$TMPDIR/files-java.txt
+java  -cp /home/friker/Systems/irankfinder/lib/commons-cli-1.3.1.jar:$TMPDIR/build:/usr/local/lib/ppl/ppl_java.jar loopkiller.irankfinder.Main ${@:2}
 
-echo $@
-
+echo ${@:2}
