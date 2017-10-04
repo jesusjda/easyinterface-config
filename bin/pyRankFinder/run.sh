2110894af6182f08bd1b724bbccaf4fa7c953e5f
Jesus Domenech <jesusjda@gmail.com>
2017-10-04 17:47:43 +0200

    updating install script

--- bin/pyRankFinder/run.sh
+++ bin/pyRankFinder/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 TMPDIR=$1/_ei_tmp
-pyRF=/home/friker/Systems/pyRankFinder/rankfinder.py
+
+${PYRANKFINDER_HOME:=/home/friker/Systems/pyRankFinder}
+pyRF=$PYRANKFINDER_HOME/rankfinder.py
 
 
 python2.7 $pyRF ${@:2} 2> $TMPDIR/errors
--- install.sh
+++ install.sh
@@ -1,28 +1,77 @@
 #!/bin/bash
 
-PWD=`pwd`
 EI_HOME=~/Systems/easyinterface
+INSTALL_EI=false
+basedir=$(dirname "$(readlink -f "$0" )")
+
+for i in "$@"; do
+    case $i in
+	--ei-home=*)
+	    EI_HOME="${i#*=}"
+	    shift 
+	    ;;
+	--install-ei)
+	    INSTALL_EI=true
+	    shift 
+	    ;;
+	*)
+	    >&2 cat  <<EOF 
+ERROR: easyinterface-config/install.sh [OPTIONS]
+
+[OPTIONS]
+
+    --ei-home=[PATH] )
+        Path to easyinterface. Default: ~/Systems/easyinterface
+
+    --install-ei )
+        Install easyinterface with personal config.
+
+EOF
+	    exit -1
+            # unknown option
+	    ;;
+    esac
+done
+
+
+install_ei(){
+    mkdir -p $EI_HOME
+    git clone https://github.com/abstools/easyinterface.git $EI_HOME
+    echo "Alias /ei \"$EI_HOME\"\n\
+\n\
+<Directory \"$EI_HOME\">\n\
+   Options FollowSymlinks MultiViews Indexes IncludesNoExec\n\
+   AllowOverride All\n\
+   Require all granted\n\
+</Directory>\n" > /etc/apache2/sites-available/easyinterface-site.conf
+    chmod -R 755 $EI_HOME
+    a2ensite easyinterface-site
+    a2enmod headers
+}
+if [ "$INSTALL_EI" == "true" ]; then
+    sudo install_ei
+fi
+
 
 if [ ! -L "$EI_HOME/server/config/jesusjda" ] ; then
-	ln -s $PWD/config $EI_HOME/server/config/jesusjda
+	ln -s $basedir/config $EI_HOME/server/config/jesusjda
 fi
 
 if [ ! -L "$EI_HOME/server/bin/jesusjda" ] ; then
-        ln -s $PWD/bin $EI_HOME/server/bin/jesusjda
+        ln -s $basedir/bin $EI_HOME/server/bin/jesusjda
 fi
 
+RND=$(date +%s)
 if [ -f "$EI_HOME/server/config/eiserver.cfg" ] ; then
-	RND=$(date +%s)
 	mv $EI_HOME/server/config/eiserver.cfg $EI_HOME/server/config/eiserver_old_$RND.cfg
 fi
 
 if [ -f "$EI_HOME/clients/web/webclient.cfg" ] ; then
-        RND=$(date +%s)
         mv $EI_HOME/clients/web/webclient.cfg $EI_HOME/clients/web/webclient_old_$RND.cfg
 fi
+RND=""
 
-
-cp $PWD/server.cfg $EI_HOME/server/config/eiserver.cfg
-cp $PWD/web.cfg $EI_HOME/clients/web/webclient.cfg
+cp $basedir/server.cfg $EI_HOME/server/config/eiserver.cfg
+cp $basedir/web.cfg $EI_HOME/clients/web/webclient.cfg
 
 
