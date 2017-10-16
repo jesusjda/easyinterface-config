fdb72126743e00f7c4809ae463888ab3f1f014ed
jesusjda <jesusjda@gmail.com>
2017-10-16 14:42:06 +0200

    pyRankFinder with multiple sets of eicommands

--- bin/pyRankFinder/run.sh
+++ bin/pyRankFinder/run.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 TMPDIR=$1/_ei_tmp
 
-${PYRANKFINDER_HOME:=/home/friker/Systems/pyRankFinder}
+${PYRANKFINDER_HOME:="/home/friker/Systems/pyRankFinder"}
 pyRF=$PYRANKFINDER_HOME/rankfinder.py
 
+mkdir -p $TMPDIR/dot
+echo "<eiout>"
 
-python2.7 $pyRF ${@:2} 2> $TMPDIR/errors
+python2.7 $pyRF ${@:2} --ei-out --dotDestination $TMPDIR/dot 2> $TMPDIR/errors
 
+i=0
+echo "<eicommands>"
+for f in $TMPDIR/dot/*.dot
+do
+    if [ -f $f ]; then
+	echo ""
+	echo "<printonconsole consoleid='graph-dot' consoletitle='Program Graph'><content><![CDATA["
+	echo $(basename $f)
+	echo "]]></content></printonconsole>"
+	echo "<printonconsole consoleid='graph-dot' consoletitle='Program Graph'>"
+	echo "<content format='svg'>"
+	dot -Tsvg $f -o $f.svg
+	cat $f.svg | sed -e :a -re 's/<!.*?>//g;/<!/N;//ba'
+	echo "</content></printonconsole>"
+	echo ""
+	i=$i + 1
+    fi
+done
+
+echo "<printonconsole consoleid='errors' consoletitle='ERRORS?'><content><![CDATA["
+echo ${@:2} --ei-out --dotDestination $TMPDIR/dot
 echo "ERRORS?"
 cat $TMPDIR/errors
-
-
-echo ${@:2}
+echo "]]></content></printonconsole>"
+echo "</eicommands>"
+echo "</eiout>"
