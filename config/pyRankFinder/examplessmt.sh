e9d619a4973bf24b756d5e2a98d9dd37fd231f24
Jesus Domenech <jesusjda@gmail.com>
2017-10-25 09:46:05 +0200

    smt2 examples

---
+++ config/pyRankFinder/examplessmt.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+localP=$1
+serverP=$2
+
+function printFile(){
+    source=$1
+    localpath=$2
+    serverpath=$3
+    filepath=$4
+    extension="${filepath##*.}"
+    filename=`basename $filepath`
+    if [[ ! -f $localpath'/'$filename ]]; then
+	curl -u $source'/'$filename > $localpath'/'$filename
+    fi
+    echo '<file name="'$filename'" url="'$serverpath'/'$filename'" />'
+}
+
+function printFolder(){
+    if [[ ! -d $1'/'$2 ]]; then
+	mkdir -p $1'/'$2
+    fi
+    echo '<folder name="'$2'">'
+}
+
+
+function preparse (){
+    localpath=$1
+    serverpath=$2
+    c=1
+    for i in "${@:3}"; do
+	if (( $c % 3 == 0 )); then
+	    printFile http://cl2-informatik.uibk.ac.at/mercurial.cgi/TPDB/raw-file/3ce4bce287e1/Integer_Transition_Systems/From_AProVE_2014/  $localpath $serverpath $i
+	fi
+	(( c++ ))
+    done
+}
+
+DATA=`curl http://cl2-informatik.uibk.ac.at/mercurial.cgi/TPDB/raw-file/3ce4bce287e1/Integer_Transition_Systems/From_AProVE_2014`
+printFolder $localP From_AProve_2014
+preparse $localP'/From_AProve_2014' $serverP'/From_AProve_2014' $DATA
+echo "</folder>"
--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 EI_HOME=~/Systems/easyinterface
 INSTALL_EI=false
 basedir=$(dirname "$(readlink -f "$0" )")
-
+EX_HOME=~/tmp/examples
+EX_REMOTE=/tmp/examples
+examples=false
 for i in "$@"; do
     case $i in
 	--ei-home=*)
@@ -13,6 +15,18 @@ for i in "$@"; do
 	--install-ei)
 	    INSTALL_EI=true
 	    shift 
+	    ;;
+	--examples=* )
+	    examples="${i#*=}"
+	    shift
+	    ;;
+	--ex-home=* )
+	    EX_HOME="${i#*=}"
+	    shift
+	    ;;
+	--ex-remote=* )
+	    EX_REMOTE="${i#*=}"
+	    shift
 	    ;;
 	*)
 	    >&2 cat  <<EOF 
@@ -25,6 +39,16 @@ ERROR: easyinterface-config/install.sh [OPTIONS]
 
     --install-ei )
         Install easyinterface with personal config.
+
+    --examples=[PATH] )
+        Script wich install examples
+
+    --ex-home=[PATH] )
+        Local path were remote examples will be download
+
+    --ex-remote=[PATH] )
+        server path where the examples will be accesible from
+
 
 EOF
 	    exit -1
@@ -50,8 +74,29 @@ install_ei(){
     a2enmod headers
     service apache2 restart
 }
+
+install_ex(){
+    mkdir -p $EX_HOME
+    $example $EX_HOME $EX_REMOTE
+    echo -e "Alias "$EX_REMOTE" \""$EX_HOME"\"\n\
+\n\
+<Directory \""$EX_HOME"\">\n\
+   Options FollowSymlinks MultiViews Indexes IncludesNoExec\n\
+   AllowOverride All\n\
+   Require all granted\n\
+</Directory>\n" > /etc/apache2/sites-available/example-site.conf
+    chmod -R 755 $EX_HOME
+    a2ensite example-site
+    service apache2 reload
+    a2enmod headers
+    service apache2 restart
+}
 if [ "$INSTALL_EI" == "true" ]; then
     install_ei
+fi
+
+if [ "$example" != "false" ]; then
+    install_ex
 fi
 
 
