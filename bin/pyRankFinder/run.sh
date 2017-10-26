dd31f48468a9dc73a77c78e462bb0e4ad2823c5f
jesusjda <jesusjda@gmail.com>
2017-10-26 05:24:48 +0200

    rankfinder includes T2

--- bin/pyRankFinder/run.sh
+++ bin/pyRankFinder/run.sh
@@ -5,6 +5,7 @@ ${PYRANKFINDER_HOME:="/home/friker/Systems/pyRankFinder"}
 pyRF=$PYRANKFINDER_HOME/rankfinder.py
 
 mkdir -p $TMPDIR/dot
+mkdir -p $TMPDIR/t2
 echo "<eiout>"
 
 python2.7 $pyRF ${@:2} --ei-out --dotDestination $TMPDIR/dot 2> $TMPDIR/errors
@@ -34,4 +35,24 @@ echo "ERRORS?"
 cat $TMPDIR/errors
 echo "]]></content></printonconsole>"
 echo "</eicommands>"
+
+
+${T2_HOME:="/opt/tools/t2"}
+pyT2=$PYRANKFINDER_HOME/runT2.py
+
+echo "<eicommands>"
+echo "<printonconsole consoleid='t2-result' consoletitle='T2 Result'><content><![CDATA["
+"./runT2.sh ${@:2}"
+python2.7 $pyT2 ${@:2} -t2 $T2_HOME -td $TMPDIR/t2 2> $TMPDIR/errors
+echo "]]></content></printonconsole>"
+echo "</eicommands>"
+
+echo "<printonconsole consoleid='errors' consoletitle='ERRORS?'><content><![CDATA["
+echo ${@:2} --ei-out --dotDestination $TMPDIR/dot
+echo "ERRORS?"
+cat $TMPDIR/errors
+echo "]]></content></printonconsole>"
+echo "</eicommands>"
+
+
 echo "</eiout>"
