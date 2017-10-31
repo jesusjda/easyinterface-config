a4a9a9cd3709bd3da38e6f0dd08c9b21e1d97e0a
Jesus Domenech <jesusjda@gmail.com>
2017-10-31 17:27:50 +0100

    configuration

--- bin/pyRankFinder/run.sh
+++ bin/pyRankFinder/run.sh
@@ -37,22 +37,4 @@ echo "]]></content></printonconsole>"
 echo "</eicommands>"
 
 
-${T2_HOME:="/opt/tools/t2"}
-pyT2=$PYRANKFINDER_HOME/runT2.py
-
-echo "<eicommands>"
-echo "<printonconsole consoleid='t2-result' consoletitle='T2 Result'><content><![CDATA["
-"./runT2.sh ${@:2}"
-python2.7 $pyT2 ${@:2} -t2 $T2_HOME -td $TMPDIR/t2 2> $TMPDIR/errors
-echo "]]></content></printonconsole>"
-echo "</eicommands>"
-
-echo "<printonconsole consoleid='errors' consoletitle='ERRORS?'><content><![CDATA["
-echo ${@:2} --ei-out --dotDestination $TMPDIR/dot
-echo "ERRORS?"
-cat $TMPDIR/errors
-echo "]]></content></printonconsole>"
-echo "</eicommands>"
-
-
 echo "</eiout>"
