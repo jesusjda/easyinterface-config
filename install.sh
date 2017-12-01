a17852baa39111440965c80d496481d0993666b5
jesusjda <jesusjda@gmail.com>
2017-12-01 23:16:53 +0100

    installer

--- install.sh
+++ install.sh
@@ -69,10 +69,11 @@ install_ei(){
    Require all granted\n\
 </Directory>\n" > /etc/apache2/sites-available/easyinterface-site.conf
     chmod -R 755 $EI_HOME
-    a2ensite easyinterface-site
-    service apache2 reload
-    a2enmod headers
-    service apache2 restart
+    echo "RUN: \
+    a2ensite easyinterface-site \
+    service apache2 reload \
+    a2enmod headers \
+    service apache2 restart"
 }
 
 install_ex(){
@@ -86,10 +87,11 @@ install_ex(){
    Require all granted\n\
 </Directory>\n" > /etc/apache2/sites-available/example-site.conf
     chmod -R 755 $EX_HOME
-    a2ensite example-site
-    service apache2 reload
-    a2enmod headers
-    service apache2 restart
+    echo "RUN: \
+    a2ensite easyinterface-site \
+    service apache2 reload \
+    a2enmod headers \
+    service apache2 restart"
 }
 if [ "$INSTALL_EI" == "true" ]; then
     install_ei
