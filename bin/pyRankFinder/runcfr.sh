d52c69d74d219471b93705011785659246d8d242
Jesus Domenech <jesusjda@gmail.com>
2018-10-24 14:07:39 +0200

    output formats parameters

--- bin/pyRankFinder/runcfr.sh
+++ bin/pyRankFinder/runcfr.sh
@@ -12,14 +12,14 @@ mkdir -p $OUTSDIR
 # mkdir -p $TMPDIR/t2
 echo "<eiout>"
 
-python3 $pyRF ${@:2} --ei-out -of fc svg -od "$OUTSDIR/" 2> "$TMPDIR/errors"
+python3 $pyRF ${@:2} --ei-out -od "$OUTSDIR/" 2> "$TMPDIR/errors"
 
 
 echo "<eicommands>"
 if [ -s "$TMPDIR/errors" ]; then
     echo "<printonconsole consoleid='errors' consoletitle='Errors'><content><![CDATA["
     echo "========== Command line ====================================="
-    echo ${@:2} --ei-out -of fc svg -od "$OUTSDIR/"
+    echo ${@:2} --ei-out -od "$OUTSDIR/"
     echo "=============================================================="
     cat "$TMPDIR/errors"
     echo "]]></content></printonconsole>"
